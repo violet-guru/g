5f6f3572e7611dd04f7450fb1ffb274dcfe3c463
Hugo <victorhugolafuente@gmail.com>
2023-01-11 08:37:28 -0800

    Update

--- src/Initial.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Initial = ({ langValue, putLang }) => (
-  <>
-  </>
-);
-
-export default Initial;
--- src/utils.js
+++
@@ -1,8 +0,0 @@
-import Box from "@mui/joy/Box";
-
-export const BaseImage = ({ ...propValues }) => (
-  <Box>
-    {/*eslint-disable-next-line jsx-a11y/alt-text*/}
-    <img {...propValues} />
-  </Box>
-);
